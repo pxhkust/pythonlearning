5ebb64628c56299f5ccc6786853ec6e04c597f47
chenpeixiu <px.chen6@gmail.com>
2021-10-24 19:20:56 +0800

    add class Square

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from ploygon import Ploygon
 from point import Point
+from square import Square
 from lane_segment import LaneSegment
 
 
 def main():
-    point_A = Point(x=1.0, y=3.0, name="A")
+    point_A = Point(x=1.0, y=1.0, name="A")
     point_B = Point(x=3.0, y=1.0, name="B")
-    point_C = Point(x=1.0, y=3.0, name="C")
-    point_D = Point(x=3.0, y=3.0, name="D")
-    lane_AB = LaneSegment(begin=point_A, end=point_B, name="AB")
-    ploygon_ABCD = Ploygon([point_A, point_B, point_C, point_D], name="ABCD")
-    print(ploygon_ABCD.sorted_by_x_and_y())
-    square_ACBD = Square([point_A, point_B, point_C, point_D], name="ACBD")
-    square_meter = meter()
-    print(square_meter)
+    point_C = Point(x=3.0, y=3.0, name="C")
+    point_D = Point(x=1.0, y=3.0, name="D")
+    square_ABCD = Square([point_A, point_B, point_C, point_D], "ABCD")
+    print(square_ABCD)
+    print(square_ABCD.side)
 
 
 if __name__ == '__main__':
--- square.py
+++ square.py
@@ -1,14 +1,15 @@
+from point import Point
 from ploygon import Ploygon
 from typing import List
 import math
 
-#问题关键在于Square怎么继承Ploygon的结果
 
 class Square(Ploygon):
-    # 还需要初始化Square吗？
-    # 没找到怎么继承父辈的结果的写法？
+    def __init__(self, points: List[Point], name: str = ""):
+        super(Square, self).__init__(points, name)
 
     @property
-    def meter(self) -> float:
-        return math.sqrt((self.Ploygon.Point.begin.x - self.Ploygon.Point.end.x) ** 2 + (self.Ploygon.Point.begin.y - self.Ploygon.Point.end.y) ** 2)
+    def side(self) -> float:
+        return math.sqrt((self.points[0].x - self.points[1].x) ** 2 +
+                         (self.points[0].y - self.points[1].y) ** 2)
 
